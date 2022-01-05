8538c7c3113e168ceca6c62329f02d2a501ee223
ruslan <ruslan.sayfutdinov@gmail.com>
2022-01-05 13:50:05 +0100

    Update Package.swift
    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "FutureKit",
+    platforms: [
+      .iOS(.v10)
+    ],
+    products: [        
+        .library(name: "FutureKit", targets: ["FutureKit"]),
+    ],
+    targets: [     
+        .target(name: "FutureKit", path: "FutureKit"),
+    ]
     dependencies: []
 )
